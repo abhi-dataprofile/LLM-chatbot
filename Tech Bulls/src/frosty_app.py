b443bf10756119929c1716d550f62394f804943e
Abhishek Jadhav <150904526+abhi-dataprofile@users.noreply.github.com>
2024-12-29 00:48:10 -0500

    Initial Commit
    After the Hackathon, Inserting the running module in development.

---
+++ Tech Bulls/src/frosty_app.py
@@ -0,0 +1,43 @@
+from openai import OpenAI
+import re
+import streamlit as st
+from prompts import get_system_prompt
+
+st.title("☃️ Tech Bulls")
+
+client = OpenAI(api_key=st.secrets.OPENAI_API_KEY)
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "system", "content": get_system_prompt()}]
+
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages:
+    if message["role"] == "system":
+        continue
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+        if "results" in message:
+            st.dataframe(message["results"])
+
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        response = ""
+        resp_container = st.empty()
+        for delta in client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
+            stream=True,
+        ):
+            response += (delta.choices[0].delta.content or "")
+            resp_container.markdown(response)
+
+        message = {"role": "assistant", "content": response}
+
+        sql_match = re.search(r"```sql\n(.*)\n```", response, re.DOTALL)
+        if sql_match:
+            sql = sql_match.group(1)
+            conn = st.connection("snowflake")
+            message["results"] = conn.query(sql)
+            st.dataframe(message["results"])
+        st.session_state.messages.append(message)
